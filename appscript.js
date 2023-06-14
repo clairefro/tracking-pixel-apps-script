043a1bb0447b31c75ce7e7b5967fc429083a677a
Claire Froelich <claire.froelich@gmail.com>
2023-06-14 16:40:50 -0400

    Update app script with setNote

--- appscript.js
+++ appscript.js
@@ -1,13 +1,13 @@
 /** Update these values */
-const TRACKING_PIXEL_SERVER_ENDPOINT = "YOUR ENDPOINT"; // endpoint for your deployed tracking pixel server
-const SPREADSHEET_ID = "19knIGGrOPS8MmiWKzGw0UkWduPxVofau6gTzJdaWva0";
+const TRACKING_PIXEL_SERVER_ENDPOINT = "xxxxxxx"; // endpoint for your deployed tracking pixel server
+const EMAIL_VALIDATION_SERVER_ENDPOINT = "xxxxxxxxx"; // endpoint for server that validates email format and mx. must accept query param "email"
+const SPREADSHEET_ID = "xxxxxx";
 const COL_SUBJECT = 3; // integer representing the absolute column number for email subject
 const COL_BODY = 4; // integer representing the absolute column number for email body
 const COL_ID = 5; // integer representing the absolute column number for placement of unqiue mail merge ID
 const COL_STATUS = COL_ID + 1; // integer representing the absolute column number for mail merge status. Defaults to right of ID col
 
 /** Expects email to column to be first column of selected range */
-/** Does not account for bounces */
 function sendEmailsWithTrackingPixel() {
   var ss = SpreadsheetApp.getActive();
   var sh = ss.getActiveSheet();
@@ -16,42 +16,72 @@ function sendEmailsWithTrackingPixel() {
 
   var activeRangeA1Notation = rg.getA1Notation();
   var startRow = getStartRow(activeRangeA1Notation);
-  Logger.log("Change!");
-  Logger.log(startRow);
 
   for (var i = 0; i < data.length; i++) {
     var recipient = data[i][0]; // Assuming recipient email addresses are in the first column of selected range
 
     // only run on rows that contain an email (ignore table headers)
+
     if (recipient.match(/@/)) {
       // Generate unique tracking pixel URL for each recipient
       const id = createID(recipient);
-      var trackingPixel = `${TRACKING_PIXEL_SERVER_ENDPOINT}?id=${id}`;
-
-      // Compose email message
-      var subject = sh.getRange(startRow + i, COL_SUBJECT).getValue();
-      var body =
-        sh.getRange(startRow + i, COL_BODY).getValue() +
-        `<img style="display:none;" src="${trackingPixel}">`;
-      Logger.log(body);
-
-      const idCell = sh.getRange(startRow + i, COL_ID);
+      // get status column cell
       const statusCell = sh.getRange(startRow + i, COL_STATUS);
-      try {
-        // Send the email
-        GmailApp.sendEmail(recipient, subject, "", { htmlBody: body });
-        idCell.setValue(id);
-        idCell.setBackground("#cfcfcf");
-        statusCell.setValue("Sent!");
-        statusCell.setBackground("#fffaa1");
-      } catch (error) {
-        // Handle the error
-        Logger.log(
-          `An error occurred when sending to '${recipient}': ${error}`
-        );
-        statusCell.setValue("ERR");
-        statusCell.setBackground("red");
+
+      /** 1. Check if email address is valid */
+      // check that email is valid. Only send to valid emails
+      var evURL = `${EMAIL_VALIDATION_SERVER_ENDPOINT}?email=${recipient}`;
+
+      var response = UrlFetchApp.fetch(evURL, {
+        method: "GET",
+        muteHttpExceptions: true,
+      });
+      var evStatusCode = response.getResponseCode();
+      var evContent = response.getContentText();
+
+      if (evStatusCode === 200) {
+        const isValid = JSON.parse(evContent).valid;
+        if (!isValid) {
+          // email bounced
+          statusCell.setValue("BOUNCED");
+          statusCell.setBackground("orange");
+        } else {
+          /** 1. Build and send email */
+          // Generate tracking pixel for email body
+          var trackingPixel = `${TRACKING_PIXEL_SERVER_ENDPOINT}?id=${id}`;
+
+          // Compose email message
+          var subject = sh.getRange(startRow + i, COL_SUBJECT).getValue();
+          var body =
+            sh.getRange(startRow + i, COL_BODY).getValue() +
+            `<img style="display:none;" src="${trackingPixel}">`;
+          Logger.log(body);
+
+          const idCell = sh.getRange(startRow + i, COL_ID);
+          try {
+            // Send the email
+            GmailApp.sendEmail(recipient, subject, "", { htmlBody: body });
+            idCell.setValue(id);
+            idCell.setBackground("#cfcfcf");
+            statusCell.setValue("SENT");
+            statusCell.setBackground("#fffaa1");
+            statusCell.setNote(`Sent: ${timestampISO()}`);
+          } catch (error) {
+            // Handle the error
+            Logger.log(
+              `An error occurred when sending to '${recipient}': ${error}`
+            );
+            statusCell.setValue("ERR");
+            statusCell.setBackground("red");
+          }
+        }
       }
+    } else {
+      statusCell.setValue(
+        "ERROR when attempting to validate email. Not sent - Try again"
+      );
+      statusCell.setBackground("yellow");
+      Logger.log("Error:", evContent, evContent);
     }
   }
 }
@@ -106,8 +136,10 @@ function doGet(e) {
 
   if (foundCell) {
     const statusCell = sheet.getRange(foundCell.row, COL_STATUS);
-    statusCell.setValue("Opened");
+    statusCell.setValue("OPENED");
     statusCell.setBackground("green");
+    const oldNote = statusCell.getNote();
+    statusCell.setNote(`${oldNote}\nOpened: ${timestampISO()}`);
   }
 
   // Return the response
@@ -129,6 +161,12 @@ function findCellById(sheet, id) {
     return { row, col };
   }
 }
+
+function timestampISO() {
+  return new Date().toISOString();
+}
+
+/** For logging webhook calls, because google apps script is weird  */
 
 // function logToSpreadsheet(sheet, message) {
 //   var timestamp = new Date().toISOString();
